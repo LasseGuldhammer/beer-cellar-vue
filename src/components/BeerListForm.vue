5dfc34d5a784b6607b4835219bd57c17264188e0
Lasse Guldhammer <lasseg@duglemmerdetaldrig.dk>
2020-03-26 11:45:32 +0100

    Contraint Validation
    Constraint Validation API fully implemented in BeerListForm

--- src/components/BeerListForm.vue
+++ src/components/BeerListForm.vue
@@ -4,16 +4,16 @@
 <template>
   <section class="beer-list__beer-form absolute">
     <form id="beer-form" ref="beerForm">
-      <input id="brewery" ref="brewery" class="beer-list__beer-input" type="text" placeholder="Brewery" v-model="newBeer.brewery" @input="checkValidity" @invalid="reportError" required>
-      <input id="name" ref="name" class="beer-list__beer-input" type="text" placeholder="Name" v-model="newBeer.name" required>
-      <input id="style" ref="style" class="beer-list__beer-input" type="text" placeholder="Style" v-model="newBeer.style" required>
-      <input id="abv" ref="abv" class="beer-list__beer-input" type="number" placeholder="Abv" v-model.number="newBeer.abv" min="0" max="99" step="0.1" required>
-      <input id="size" ref="size" class="beer-list__beer-input" type="number" placeholder="Size" v-model.number="newBeer.size" min="1" required>
-      <input id="quantity" ref="quantity" class="beer-list__beer-input" type="number" placeholder="Quantity" v-model.number="newBeer.quantity" min="1" required>
+      <input id="brewery" ref="brewery" class="beer-list__beer-input" type="text" placeholder="Brewery" v-model="newBeer.brewery" @input="checkValidity" @invalid="reportError" data-error="Please enter a name for the brewery" required>
+      <input id="name" ref="name" class="beer-list__beer-input" type="text" placeholder="Name" v-model="newBeer.name" @input="checkValidity" @invalid="reportError" data-error="Please enter a name for the beer" required>
+      <input id="style" ref="style" class="beer-list__beer-input" type="text" placeholder="Style" v-model="newBeer.style" @input="checkValidity" @invalid="reportError" data-error="Please enter a beer style" required>
+      <input id="abv" ref="abv" class="beer-list__beer-input" type="number" placeholder="Abv" v-model.number="newBeer.abv" @input="checkValidity" @invalid="reportError" min="0" max="99" step="0.1" data-error="Please enter the beer's alcohol content" required>
+      <input id="size" ref="size" class="beer-list__beer-input" type="number" placeholder="Size" v-model.number="newBeer.size" @input="checkValidity" @invalid="reportError" min="1" data-error="Please enter the size of the beer" required>
+      <input id="quantity" ref="quantity" class="beer-list__beer-input" type="number" placeholder="Quantity" v-model.number="newBeer.quantity" @input="checkValidity" @invalid="reportError" min="1" data-error="Please enter the number of beers" required>
       <input id="date" ref="date" class="beer-list__beer-input" type="date" placeholder="Date" v-model="newBeer.date" :max="maxDate">
       <input id="minimum" ref="minimum" class="beer-list__beer-input" type="number" placeholder="Minimum Age" v-model="newBeer.minimumAge">
-      <button type="submit" @click.prevent="saveBeer" v-if="mode === 'editBeer'">Save beer</button>
-      <button type="submit" @click.prevent="addBeer" v-else>Add new beer</button>
+      <button type="submit" @click="saveBeer" v-if="mode === 'editBeer'">Save beer</button>
+      <button type="submit" @click="addBeer" v-else>Add new beer</button>
     </form>
   </section>
 </template>
@@ -58,31 +58,25 @@ export default {
   },
   methods: {
     checkValidity: function (e) {
-      console.log(e)
+      e.target.setCustomValidity('')
+      e.target.checkValidity()
     },
     reportError: function (e) {
-
-    },
-    validate: function () {
-      var refs = this.$refs
-      if (!refs.beerForm.checkValidity()) {
-        if (!refs.brewery.checkValidity()) {
-          refs.brewery.setCustomValidity('Please enter a name for the brewery')
-        } else {
-          refs.brewery.setCustomValidity('')
-        }
+      if (e.target.value === '') {
+        e.target.setCustomValidity(e.target.dataset.error)
       }
-      return refs.beerForm.checkValidity()
+    },
+    validateForm: function () {
+      return this.$refs.beerForm.checkValidity()
     },
     saveBeer: function () {
-      if (this.validate()) {
+      if (this.validateForm()) {
         var beer = JSON.parse(JSON.stringify(this.newBeer))
-        // console.log(beer)
         this.$emit('save-beer', beer)
       }
     },
     addBeer: function () {
-      if (this.validate()) {
+      if (this.validateForm()) {
         this.$emit('add-beer', this.newBeer)
         this.newBeer = {}
       }
